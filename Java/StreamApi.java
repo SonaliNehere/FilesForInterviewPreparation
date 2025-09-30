af849eebb8cb2475d3caceb052482556f2f457f0
Sonali Sopan Nehere <83354024+SonaliNehere@users.noreply.github.com>
2025-09-30 16:15:30 +0530

    Add files via upload

--- Java/StreamApi.java
+++ Java/StreamApi.java
@@ -4,11 +4,75 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.OptionalInt;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+class Person{
+	 int age;
+	
+	Person(int age){
+		this.age = age;
+	}
+	
+	 int getAge() {
+		return age;
+	}
+	 
+}
+
+class Emp{
+	String department;
+	String name;
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public Emp(String name, String department) {
+		this.name = name;
+		this.department = department;
+	}
+	
+	
+}
+
+class Transaction{
+	String date;
+	int amount;
+	public Transaction(String date, int amount) {
+		this.amount = amount;
+		this.date = date;
+	}
+	
+	public int getAmount() {
+		return amount;
+	}
+
+	public String getDate() {
+		return date;
+	}
+}
+
 public class StreamApi {
+	
+	public static boolean isPrime(int number) {
+		  if (number <= 1) {
+		    return false;
+		  }
+		  for (int i = 2; i <= Math.sqrt(number); i++) {
+		    if (number % i == 0) {
+		        return false;
+		    }
+		  }
+		  return true;
+		}
 
 	public static void main(String[] args) {
 //		🔥 What is Stream API?
@@ -142,11 +206,11 @@ public class StreamApi {
 //        Map<Integer, List<String>> grouped = list.stream()
 //                .collect(Collectors.groupingBy(String::length));
 //        System.out.println(grouped);
-		
+//		
 //		List<String> names = List.of("Sonali", "Snehal", "Sam");
-//		Map<Integer, List<String>> grouped = names.stream()
+//		Map<Integer, List<String>> grouped1 = names.stream()
 //		    .collect(Collectors.groupingBy(name -> name.length()));
-//		System.out.println(grouped);
+//		System.out.println(grouped1);
 
 //		Join names with comma.
 //		String joinByComma = Arrays.asList("Sona", "Sonali", "Rani", "Snehal", "Sejal", "Mom", "Dad")
@@ -270,12 +334,12 @@ public class StreamApi {
 //			parallelStream() is a feature of the Java Stream API that allows stream operations to be executed in parallel—across multiple threads—using the ForkJoinPool under the hood.
 //
 //			🔹 Usage Example:
-
-			     List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
-			     int sum = numbers.parallelStream()
-			                         .mapToInt(i -> i * 2)
-			                         .sum();
-			      System.out.println("Sum: " + sum);
+//
+//			     List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+//			     int sum = numbers.parallelStream()
+//			                         .mapToInt(i -> i * 2)
+//			                         .sum();
+//			      System.out.println("Sum: " + sum);
 			
 //			✅ Advantages / Uses of Parallel Streams:
 //			Advantage	Explanation
@@ -305,7 +369,96 @@ public class StreamApi {
 		
 		
 		
-
+		
+		////////////////practice code
+//		Q. Find the longest string in a list of strings using Java streams:
+//		Optional<String> longestString = Arrays.asList("SOnali", "Sona", "KArn", "Sahas", "Amit").stream().max((a, b) -> Integer.compare(a.length(), b.length()));
+//		longestString.ifPresent(System.out :: println);
+		
+//		Q. Calculate the average age of a list of Person objects using Java streams:
+//		double avg = Arrays.asList(new Person(19), new Person(25)).stream().mapToInt(Person::getAge).average().orElse(0.0); // default if list is empty;
+//		System.out.println("Average age: " + avg);
+		
+//		Q. Check if a list of integers contains a prime number using Java streams:
+//		boolean primeExists = Arrays.asList(1,2,3,4,5).stream().anyMatch(x -> isPrime(x));
+//		System.out.println("primeExists : " + primeExists);
+		
+//		Q. Merge two sorted lists into a single sorted list using Java streams:
+//		List<Integer> sorted = Arrays.asList(Arrays.asList(12, 23, 45, 67), Arrays.asList(7, 8, 20, 40, 60, 70)).stream().flatMap(List::stream).sorted().collect(Collectors.toList());
+//		System.out.println(sorted);
+//		
+//		List<Integer> l1 = Arrays.asList(10,20,30,40);
+//		<Integer> l2 = Arrays.asList(5,15,20,25,45);
+//		List<Integer> merge = Stream.concat(l1.stream(), l2.stream()).sorted().toList();
+//		System.out.println(merge);
+		
+		
+//		Q. Find the intersection of two lists using Java streams:
+//		List<Integer> l1 = Arrays.asList(10,20,30,40);
+//		List<Integer> l2 = Arrays.asList(5,15,20,25, 30, 45);
+//		List<Integer> intersection = l2.stream().filter(l1::contains).collect(Collectors.toList());
+//		System.out.println("merge : " + intersection);
+		
+//		Q. Remove duplicates from a list while preserving the order using Java streams:
+//		List<Integer> removeDuplicates = Arrays.asList(1,2,2,3,4,4,5,6).stream().distinct().toList();
+//		System.out.println("removeDuplicates : " + removeDuplicates);
+		
+//		Q. Given a list of transactions, find the sum of transaction amounts for each day using Java streams:
+//		List<Transaction> transactions = Arrays.asList(
+//			    new Transaction("2022-01-01", 100),
+//			    new Transaction("2022-01-01", 200),
+//			    new Transaction("2022-01-02", 300),
+//			    new Transaction("2022-01-02", 400),
+//			    new Transaction("2022-01-03", 500)
+//			);
+//		
+//		Map<String, Integer> sum = transactions.stream().collect(Collectors.groupingBy(Transaction::getDate, Collectors.summingInt(Transaction::getAmount)));
+//		System.out.println(sum);
+		
+//		Q. Find the kth smallest element in an array using Java streams:
+//		int k = 2;
+//		Optional<Integer> kth = Arrays.asList(23, 45, 12,34,3,67,88).stream().sorted().skip(k-1).findFirst();
+//		kth.ifPresent(System.out::println);
+		
+//		Q. Implement a method to partition a list into two groups based on a predicate using Java streams:
+//		Map<Boolean , List<Integer>> partition = Arrays.asList(12,34,56,999,677,43,5,7777,23,11,2222).stream().collect(Collectors.partitioningBy(x -> x>100));
+//		System.out.println(partition);
+//		System.out.println(partition.get(true));
+//		System.out.println(partition.get(false));
+		
+//		🔹 Medium Level
+//		3. Count frequency of each character in a string
+//		String str = "Sonali Nehere";
+//		Map<Character, Long> frequency =  str.chars().mapToObj(c -> (char)c).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+//		System.out.println("frequency : " + frequency);
+		
+//		4. Find the second highest number in a list
+//		int res = Arrays.asList(34,67,89,9,45,67,34,21,2).stream().sorted(Comparator.reverseOrder()).skip(1).findFirst().orElse(-1);
+//		System.out.println("res : " +  res);
+		
+//		5. Merge two lists and remove duplicates
+//		List<Integer> l1 = Arrays.asList(12,56,78,90,34,56,45);
+//		List<Integer> l2 = Arrays.asList(56, 99,777,6,2,34, 6);
+//		List<Integer> res = Stream.concat(l1.stream(), l2.stream()).distinct().toList();
+//		System.out.println("res : " + res);
+		
+//		6. Find the employee with the highest salary (custom object)
+//		List<Person> persons = Arrays.asList(new Person(12), new Person(89), new Person(88));
+//		int p = persons.stream().map(Person::getAge).sorted(Comparator.reverseOrder()).findFirst().orElse(-1);
+//		System.out.println(p);
+		
+//		7. Group employees by department
+		List<Emp> emps = Arrays.asList(
+			    new Emp("Sonali", "Developer"),
+			    new Emp("Snehal", "Devops"),
+			    new Emp("Vikas", "Cyber"),
+			    new Emp("Sakshi", "Developer"),
+			    new Emp("Shradha", "Tester")
+			);
+		Map<String, List<String>> map = emps.stream().collect(Collectors.groupingBy(Emp::getDepartment, Collectors.mapping(Emp::getName, Collectors.toList()))); 
+		System.out.println(map);
+		 
+		
 	}
 
 }
